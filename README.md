5ff0f54aa3d4f4e0e10f8044da55395e1f928a3c
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2024-11-07 13:16:23 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Note App
+
+## (React + TS)
+
+### this project uses bootstrap & react-router-dom
+
+advanced note app with this features:
+
+- add,delete,edit notes
+- add,delete,edit category
+- save notes on localStorage
+- support for markdown
+
+#### to run the project just run this command
+
+```bash
+npm run dev
+```
---
+++ src/components/NoteLayout.tsx
@@ -0,0 +1,26 @@
+import {
+  Navigate,
+  Outlet,
+  useOutletContext,
+  useParams,
+} from "react-router-dom";
+import { Note } from "../App";
+
+type NoteLayoutProps = {
+  notes: Note[];
+};
+
+function NoteLayout({ notes }: NoteLayoutProps) {
+  const { id } = useParams();
+  const note = notes.find((note) => note.id === id);
+  if (note == null) return <Navigate to="/" replace />;
+
+  return <Outlet context={note} />;
+}
+
+export default NoteLayout;
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useNote() {
+  return useOutletContext<Note>();
+}
